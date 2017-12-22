78d99fc38bbb5826d850a2f9be2b66deb022b236
Ronald Leppink <ronaldleppink@gmail.com>
2017-12-22 17:04:10 +0100

    Add status code check to litebit checker

--- src/litebit_checker.py
+++ src/litebit_checker.py
@@ -5,15 +5,18 @@ import requests
 import arbotrator
 
 
-litebit_page = requests.get("https://www.litebit.eu/en/buy/ripple").text
+response = requests.get("https://www.litebit.eu/en/buy/ripple")
+litebit_page = response.text
 
 
 with open("result", "w") as result_file:
     result_file.write(litebit_page.encode("ascii", "ignore").decode("ascii"))
 
 
-if ("Checking your browser" in litebit_page) or \
-   ("The web server reported a bad gateway error" in litebit_page):
+if response.status_code is not 200 or \
+   ("Checking your browser" in litebit_page) or \
+   ("The web server reported a bad gateway error" in litebit_page) or \
+   ("Connection timed out" in litebit_page):
     # Don't bother with CloudFlare protection thing
     print("Foiled!")
     sys.exit(0)
