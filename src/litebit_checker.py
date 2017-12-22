dd1d15beea373124338ad0164bfdb8bd5c07ae44
Ronald Leppink <ronaldleppink@gmail.com>
2017-12-22 15:18:32 +0100

    Fix unicode/ascii problem

--- src/litebit_checker.py
+++ src/litebit_checker.py
@@ -7,15 +7,20 @@ import arbotrator
 
 litebit_page = requests.get("https://www.litebit.eu/en/buy/ripple").text
 
+
+with open("result", "w") as result_file:
+    result_file.write(litebit_page.encode('ascii', 'ignore'))
+
+
 if ("Checking your browser" in litebit_page):
     # Don't bother with CloudFlare protection thing
     sys.exit(0)
 
 
-if ("0 available" not in litebit_page) and \
+if (">0 available" not in litebit_page) and \
    ("Due to maintenance on other exchanges" not in litebit_page):
         arbotrator.send_message(
             """
 LiteBit lijkt Ripple/XRP beschikbaar te hebben!
-https://www.litebit.eu/nl/kopen/ripple"
+https://www.litebit.eu/nl/kopen/ripple
             """)
