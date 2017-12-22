d22c4674f27d8f0d2e58fe6fbfc3b788dfb72cf1
Ronald Leppink <ronaldleppink@gmail.com>
2017-12-22 12:18:51 +0100

    Initial valet setup

---
+++ src/valet.py
@@ -0,0 +1,54 @@
+from flask import Flask
+
+from flask import abort
+from flask import request
+
+import arbotrator
+import ticker
+
+
+app = Flask("CoinpraatValet")
+
+
+def get_webhook_token():
+    with open("../private/webhook_token", "r") as webhook_token_file:
+        return webhook_token_file.read().strip()
+
+
+def auth_path(url):
+    return "/{}/{}/".format(get_webhook_token(), url)
+
+
+@app.route(auth_path("updates"), methods=["POST"])
+def listen_closely():
+    if "message" not in request.get_json():
+        abort(405)
+
+    if request.get_json()["message"]["text"].startswith("/prijs"):
+        coin_id = request.get_json()["message"]["text"].split(" ")[1]
+
+        result = ticker.get_api_ticker_result(coin_id)
+        arbotrator \
+            .send_message("Hi {}, {} is op dit moment *USD*: {}"
+                          .format(
+                              request.get_json()
+                              ["message"]["from"]["first_name"],
+                              result["name"],
+                              result["price_usd"]))
+
+    return ""
+
+
+@app.errorhandler(404)
+def unknown(error):
+    abort(401)
+
+
+if __name__ == "__main__":
+    context = ("../private/cert.cert", "../private/key.key")
+    app.run(
+        host="127.0.0.1",
+        port=5000,
+        ssl_context=context,
+        threaded=True,
+        debug=True)
