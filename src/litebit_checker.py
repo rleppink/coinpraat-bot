691b786b52bd6793bb1700e45d4459e54b5595dd
Ronald Leppink <ronaldleppink@gmail.com>
2017-12-22 12:37:18 +0100

    Add cloudflare protection exception

--- src/litebit_checker.py
+++ src/litebit_checker.py
@@ -1,9 +1,16 @@
+import sys
+
 import requests
 
 import arbotrator
 
 
 litebit_page = requests.get("https://www.litebit.eu/en/buy/ripple").text
+
+if ("Checking your browser" in litebit_page):
+    # Don't bother with CloudFlare protection thing
+    sys.exit(0)
+
 
 if ("0 available" not in litebit_page) and \
    ("Due to maintenance on other exchanges" not in litebit_page):
