21363f113fb4aadb15119b9669810f2f0d5e9b08
Ronald Leppink <ronaldleppink@gmail.com>
2017-12-23 15:19:37 +0100

    Refactor litebit checker to accept any coin

--- src/litebit_checker.py
+++ src/litebit_checker.py
@@ -5,29 +5,40 @@ import requests
 import arbotrator
 
 
-def get_litebit_ripple():
-    return requests.get("https://api.litebit.eu/market/xrp")
+def get_litebit_coin(coin_id):
+    return requests.get("https://api.litebit.eu/market/{}".format(coin_id))
 
 
 if __name__ == "__main__":
-    response = get_litebit_ripple()
+    if len(sys.argv) is 1:
+        print("Need an argument")
+        sys.exit(1)
+
+    coin_id = sys.argv[1]
+    response = get_litebit_coin(coin_id)
+
     if response.json()["success"] is False:
         sys.exit(0)
 
     if float(response.json()["result"]["available"]) > 0:
         message = \
             """
-🚨 *RIPPLES!* 🚨
+🚨 *{}S* 🚨
 
-LiteBit heeft Ripple/XRP beschikbaar!
+LiteBit heeft weer {}/{} beschikbaar!
 
 Voorraad: {}
 Koopprijs *EUR*: €{}
 Verkoopprijs *EUR*: €{}
 
-https://www.litebit.eu/nl/kopen/ripple
-            """.format(response.json()["result"]["available"],
+https://www.litebit.eu/nl/kopen/{}
+            """.format(
+                       response.json()["result"]["name"].upper(),
+                       response.json()["result"]["name"],
+                       response.json()["result"]["abbr"],
+                       response.json()["result"]["available"],
                        response.json()["result"]["buy"],
-                       response.json()["result"]["sell"])
+                       response.json()["result"]["sell"],
+                       response.json()["result"]["name"].lower())
 
         arbotrator.send_message(message)
