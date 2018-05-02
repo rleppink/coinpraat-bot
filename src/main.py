3cec7fa47d3334f94f11f5f8b31459556000d2fc
rleppink <ronaldleppink@gmail.com>
2018-05-02 16:50:47 +0200

    Neated up main.py

--- src/main.py
+++ src/main.py
@@ -7,36 +7,36 @@ import telegram
 def main():
     config = shared.config.Config()
 
-    telegram_incoming_queue = multiprocessing.Queue(config.queue_size)
+    price_check_queue = multiprocessing.Queue(config.queue_size)
     telegram_outgoing_queue = multiprocessing.Queue(config.queue_size)
 
-    telegram_incoming_handler = \
-            multiprocessing.Process(
-                    target = telegram.incoming.handler,
-                    args = (telegram_incoming_queue, config,))
-
-    telegram_outgoing_handler = \
-            multiprocessing.Process(
-                    target = telegram.outgoing.handler,
-                    args = (telegram_outgoing_queue,))
-
-    telegram_incoming_handler.start()
-    telegram_outgoing_handler.start()
+    multiprocessing.Process(
+        target = telegram.incoming.handler,
+        args = (
+            price_check_queue,
+            config,
+        )).start()
 
     multiprocessing.Process(
-        target=dummy_handler,
+        target = telegram.outgoing.handler,
+        args = (
+            telegram_outgoing_queue,
+        )).start()
+
+    multiprocessing.Process(
+        target=queue_glue,
         args=(
-            telegram_incoming_queue,
+            price_check_queue,
             telegram_outgoing_queue,
         )).start()
 
 
 
-def dummy_handler(incoming_queue, outgoing_queue):
-    print("[MAIN] Started dummy handler...")
+def queue_glue(in_queue, out_queue):
+    print("[MAIN] Started queue glue...")
     while True:
-        message = incoming_queue.get()
-        outgoing_queue.put(message)
+        message = in_queue.get()
+        out_queue.put(message)
 
 
 if __name__ == "__main__":
