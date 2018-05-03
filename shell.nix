f6d83109ab58818eea818224d37cd6da92c227af
rleppink <ronaldleppink@gmail.com>
2018-05-03 09:41:58 +0200

    Update nix shell to be more flexible

--- shell.nix
+++ shell.nix
@@ -1,4 +1,14 @@
 with import <nixpkgs> {};
 
-(python36.withPackages
-  (ps: with ps; [ flask requests tzlocal yapf ])).env
+stdenv.mkDerivation {
+    name = "coinpraat-bot";
+    buildInputs = [
+        python36
+        python36Packages.requests
+        python36Packages.yapf
+        python36Packages.tzlocal
+    ];
+    shellHook = ''
+        alias run="python src/main.py"
+    '';
+}
